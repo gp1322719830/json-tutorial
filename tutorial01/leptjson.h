45b97a4fd53de0fefc60a39921889261fda680a0
gp1322719830 <1322719830@qq.com>
2020-04-13 09:12:10 +0800

    test one commit

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,17 +1,18 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
+//JSON类型
 typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
 
 typedef struct {
-    lept_type type;
+    lept_type type;  //JSON值
 }lept_value;
 
-enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+enum {  //返回状态只能
+    LEPT_PARSE_OK = 0,  //解析成功
+    LEPT_PARSE_EXPECT_VALUE,  //只含有空白
+    LEPT_PARSE_INVALID_VALUE,  //无效值， 非null、boolean字面量
+    LEPT_PARSE_ROOT_NOT_SINGULAR  //若在值和空白之后还有其他字符
 };
 
 int lept_parse(lept_value* v, const char* json);
